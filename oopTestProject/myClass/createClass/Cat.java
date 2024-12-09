dc5ef1356b569edd1e24c8c031d33f73a50380c5
JinsuNa <shs1401009@gmail.com>
2024-12-09 18:11:05 +0900

    Create Cat.java

---
+++ oopTestProject/myClass/createClass/Cat.java
@@ -0,0 +1,15 @@
+package myClass.createClass;
+
+public class Cat {
+    public String catName;
+    public String catColor;
+
+    public Cat() {
+
+    }
+
+    public Cat(String name, String color){  //--> overloading 이라고 한다.
+        catName = name;
+        catColor = color;
+   }
+}
