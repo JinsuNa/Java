0250189eeb493a8fcd61c388ccb3adf0fd0f3440
JinsuNa <shs1401009@gmail.com>
2024-12-12 18:27:36 +0900

    Add files via upload

---
+++ oopTestProject/myClass/composition/Engine.java
@@ -0,0 +1,12 @@
+package myClass.composition;
+
+public class Engine {
+    private final int power = 2000;
+
+    public int getPower() {
+        return power;
+    }
+}
+
+
+//엔진에 파워를 get만하여 power를 return 했다.
